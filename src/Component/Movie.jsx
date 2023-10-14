8e09ad4b0dc3226c56a5099e4d1863e1c5b04767
gracelee5 <113881477+gracelee5@users.noreply.github.com>
2023-10-15 00:59:25 +0900

    commit

--- src/Component/Movie.jsx
+++ src/Component/Movie.jsx
@@ -1,22 +1,44 @@
-import React from 'react';
-import '/Users/gracelee/Desktop/umc-workbook/umc-workbook1/src/index.js'
-function Movie(props) {
-  // Props에서 데이터 추출
-  const { title, posterPath, voteAverage} = props;
-  const movieStyle={
-    color:'white'
+import React, { useState } from 'react';
+
+
+function Movie({ title, posterPath, voteAverage, overview }) {
+  const [isHovered, setIsHovered] = useState(false);
+
+  const movieStyle = {
+    color: 'white',
+  };
+
+  const handleMouseEnter = () => {
+    setIsHovered(true);
+  };
+
+  const handleMouseLeave = () => {
+    setIsHovered(false);
   };
 
   return (
-    <div className="movie" style={movieStyle}>
-      <img src={`https://image.tmdb.org/t/p/w1280/${posterPath}`}height="200px" width="150px" alt={title} />
+    <div
+      className="movie"
+      style={movieStyle}
+      onMouseEnter={handleMouseEnter}
+      onMouseLeave={handleMouseLeave}
+    >
+      <img
+        src={`https://image.tmdb.org/t/p/w1280/${posterPath}`}
+        alt={title}
+        style={{ maxWidth: '100%', height: 'auto', flex: '1' }}
+      />
       <div className="content">
-        <p>{title}   {voteAverage}</p>
+        <p>{title} {voteAverage}</p>
+        {isHovered && (
+          <div className="overlay">
+            <p>{title}</p>
+            <p>{overview}</p> 
+          </div>
+        )}
       </div>
-      
     </div>
   );
 }
 
-  
-  export default Movie
+export default Movie;
